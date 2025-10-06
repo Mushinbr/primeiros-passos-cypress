d8c097ae36291107ab5f115dc5b46931e94704e8
Andrei Rocha <you@example.com>
2025-10-06 18:25:10 +0900

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+
+
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2)').type('Test')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2)').type('Test')
+    cy.get('.oxd-button').click()
+    
+})
+})
